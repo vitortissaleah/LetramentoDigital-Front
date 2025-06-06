a4833e19d77a79b5f7fac41394eead442363f8f7
Vitor Tissaleah <43892866+vitortissaleah@users.noreply.github.com>
2025-06-06 11:15:34 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 80,         // Porta alterada para 80
+    host: true        // Permite acesso externo (por IP público ou domínio)
+  }
 })
